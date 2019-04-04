4039d9dbbbdc1b18f04c8386accbd53dfb5493c2
Himanshu <himanshurnjn04@gmail.com>
2019-04-04 15:53:29 +0530

    Added Kubernetes Manifests File

--- client/src/app/auth.service.ts
+++ client/src/app/auth.service.ts
@@ -15,21 +15,21 @@ export class AuthService {
 
   login(obj) {
     const data = JSON.stringify(obj)
-    return this.http.post("http://localhost:8000/api/user/login", data);
+    return this.http.post("http://chat-app-server:8000/api/user/login", data);
   }
 
   register(obj) {
     const data = JSON.stringify(obj)
-    return this.http.post("http://localhost:8000/api/user/new", data);
+    return this.http.post("http://chat-app-server:8000/api/user/new", data);
   }
 
   getAllUsers() {
-    return this.http.get("http://localhost:8000/api/users");
+    return this.http.get("http://chat-app-server:8000/api/users");
   }
 
   getAllUserMessages(obj) {
     const data = JSON.stringify(obj)
-    return this.http.post("http://localhost:8000/api/users/getAllMessages", data);
+    return this.http.post("http://chat-app-server:8000/api/users/getAllMessages", data);
   }
 
   getToken() {
